3eb232ec29a2632275b6bd7b13172907ccc33d12
Porto888 <guilhermepstk@hotmail.com>
2023-10-10 21:48:14 -0300

    Configurando a cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,15 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '9mpgkq',
   chromeWebSecurity: false,
   e2e: {
     baseurl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
     },
-    defaultCommandTimeout: 45000,
+    defaultCommandTimeout: 60000,
+
 
   },
 
