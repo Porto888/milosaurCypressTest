84c48fdfa748d27257600d74ffceda5a3341bee5
Porto888 <guilhermepstk@hotmail.com>
2023-10-14 11:50:20 -0300

    Configurando cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   projectId: '9mpgkq',
   chromeWebSecurity: false,
   e2e: {
+    setupNodeEvents(on, config) {
+      require('plugin@cypress/grep/src/plugin')(config)
+      return config
+    },
     baseurl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
